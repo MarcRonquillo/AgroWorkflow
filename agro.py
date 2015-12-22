a2341da1d5d78bd3dafe25b78895150dcc07d462
MarcRonquillo <marc.ronq@gmail.com>
2015-12-22 10:22:58 +0100

    agro.py

---
+++ agro.py
@@ -0,0 +1,37 @@
+import sys, os
+from qgis.core import *
+#from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+QgsApplication.setPrefixPath("/usr/bin", True)
+app= QgsApplication([],True)
+QgsApplication.initQgis()
+
+from os.path import expanduser
+home=expanduser("~")
+
+sys.path.append(r"/home/postpro/.qgis2/python/plugins")
+#import processing
+
+from processing.core.Processing import Processing
+Processing.initialize()
+from processing.tools import *
+
+arguments=[]
+
+def main():
+	global arguments
+	for arg in sys.argv:
+		arguments.append(arg)
+	print (str(arguments))
+
+	
+
+	general.runalg("qgis:convexhull",r"/media/sf_shared_folder_centos/punts.shp",None,0,r"/media/sf_shared_folder_centos/puntsOut.shp")
+
+
+
+
+main()
+
+QgsApplication.exitQgis()
