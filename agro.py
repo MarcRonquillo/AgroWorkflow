ab7a0b3ecfe96e25c6834281d693cef702e88f80
postpro <postpro@localhost.localdomain>
2015-12-22 13:14:49 +0100

    Imports definidos y llamadas a saga,grass y qgis funcionando correctamente
    En el código encontraréis indicado qué lineas son necesarias para hacer
    todos los imports correctamente.
    Después en el main veréis que he probado de ejecutar algoritmos de qgis,
    saga y grass. Todos funcionan correctamente (saga da un error cuando
    python intenta cerrar el código, pero es cuando todo ya se ha ejecutado
    y generado, así que da absolutamente igual). Únicamente falta otb por
    configurar correctamente.

--- agro.py
+++ agro.py
@@ -1,4 +1,5 @@
-# Este es un archivo de pruebas de Marc Ronquillo
+# Marc Ronquillo. 
+#Aqui empiezan los imports necesarios
 
 import sys, os
 from qgis.core import *
@@ -8,14 +9,13 @@ QgsApplication.setPrefixPath("/usr/bin", True)
 app= QgsApplication([],True)
 QgsApplication.initQgis()
 
-#from os.path import expanduser
-#home=expanduser("~")
-
-sys.path.append(r"/home/postpro/.qgis2/python/plugins")
+sys.path.append("/home/postpro/.qgis2/python/plugins")
 
 from processing.core.Processing import Processing
 Processing.initialize()
-from processing.tools import *
+import processing.tools as proctools
+
+#Aqui terminan los imports necesarios
 
 arguments=[]
 
@@ -26,12 +26,14 @@ def main():
 	print (str(arguments))
 
 	
-
-	general.runalg("qgis:convexhull",r"/media/sf_shared_folder_centos/punts.shp",None,0,r"/media/sf_shared_folder_centos/puntsOut.shp")
-
-
+#qgis y grass van bien. Saga tambien crea el output correctamente, pero al ejecutarlo veréis que indica "segmentation fault". Es al cerrar el script, cuando ya está todo ejecutado y creado (nos la pela)
+ 
+	#proctools.runalg("qgis:convexhull",r"/media/sf_shared_folder_centos/punts.shp",None,0,r"/media/sf_shared_folder_centos/puntsOut.shp")
+	#proctools.general.runalg("grass:v.to.points","/home/postpro/test/line.shp","100",False,False,False,None,-1,0.0001,0,"/home/postpro/test/point.shp")
+	proctools.general.runalg("saga:simplefilter","/media/sf_shared_folder_centos/pcd.tif",1,0,5,"/media/sf_shared_folder_centos/saga_smooth.tif")
 
 
 main()
 
 QgsApplication.exitQgis()
+
