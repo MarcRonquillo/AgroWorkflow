a640ddd3657d1aa781fdcc6c60020cb441ba84ec
postpro <postpro@localhost.localdomain>
2015-12-23 13:05:33 +0100

    Cambios en Bulk y creacion basic_processing

--- EC/Bulk.py
+++ EC/Bulk.py
@@ -1,49 +1,65 @@
-# Bulk: Clase que se define como un grupo de parcelas. Seinicializa con un raster ya cortado y el shape que define las parcelas que aparecen en el mismo. Contiene métodos para el procesado de las imágenes que lo definen
+# Bulk: Clase que se define como un grupo de parcelas. Seinicializa con un raster ya cortado y el shape que define las parcelas que aparecen en el mismo. Contiene metodos para el procesado de las imagenes que lo definen
 
 
 # TODO Generar los imports necesarios para trabajar con raster y shape
+# Marc Ronquillo. 
+#Aqui empiezan los imports necesarios
 
-import sys
+import sys, os
 from qgis.core import *
 from PyQt4.QtGui import *
-app= QApplication([])
+
 QgsApplication.setPrefixPath("/usr/bin", True)
+app= QgsApplication([],True)
 QgsApplication.initQgis()
 
-sys.path.append("/home/postpro/.qgis2/python/plugins/")
-import processing
+sys.path.append("/home/postpro/.qgis2/python/plugins")
 
 from processing.core.Processing import Processing
 Processing.initialize()
-from processing.tools import *
+import processing.tools as proctools
+
+#Aqui terminan los imports necesarios
 
 
 class Bulk:
 
 
+
 	def __init__(self, raster,shape):
+
 		self.raster = raster
 		self.shape = shape
-		self.attributes = get_attributes()
+		self.attributes = self.get_attributes()
+		print self.attributes
 
 	def get_attributes(self):
 		
 		# TODO Read the shape and the associated attribute table
 
+		print raster
+
+		attributes = raster.split("/")
 
 		return attributes
+
 
 	def process_raster(self):
 
 		#Define the processing based on the shape information
 
-		#Basic Processing
+		#Basic Processing (RGB, )
 
-		basicProcessing(raster,shape)
-
-		
+		[RGB, PCD, Zonificado] = basicProcessing(raster,shape)
 
 
+
+
+
+raster = "/path/B1/raster"
+shape = "/path/B1/shape"
+
+Bulk1 = Bulk(raster,shape)
 
 
 
---
+++ EC/basic_processing.py
@@ -0,0 +1,27 @@
+import sys, os
+from qgis.core import *
+from PyQt4.QtGui import *
+
+QgsApplication.setPrefixPath("/usr/bin", True)
+app= QgsApplication([],True)
+QgsApplication.initQgis()
+
+sys.path.append("/home/postpro/.qgis2/python/plugins")
+
+from processing.core.Processing import Processing
+Processing.initialize()
+import processing.tools as proctools
+
+#return RGB, PCD, Zonificado
+#raster= /path/B1/raster
+def basic_processing(raster, shape):
+	output=[]
+	pathRGB=get_path(raster,"RGB")
+	pathPCD=get_path(raster,"PCD")
+	pathZonificado=get_path(raster,"Zonificado")
+
+	return output
+
+def get_path(raster, file):
+	pathSlash=raster.split("/")
+
